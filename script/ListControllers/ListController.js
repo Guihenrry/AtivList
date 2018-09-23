ef1ee62f32db5517313aaec24308246322e9619b
guihenrry <guilhermehenry60@gmail.com>
2018-09-23 17:32:12 -0300

    add method that remove ativ

--- script/ListControllers/ListController.js
+++ script/ListControllers/ListController.js
@@ -2,20 +2,35 @@ class ListController {
 
 	constructor(){
         this.addAtivEl = document.querySelector('.add-ativ');
-        this.listEl = document.querySelector('.list')
+        this.listEl = document.querySelector('.list');
         this.btnAdd = document.querySelector('.btn-add');
         this.onBtnAdd();
     }
 
+    btnDel(item){
+
+        item.querySelector('.btn-del').addEventListener('click', ev=>{
+
+            item.remove();
+
+        })
+
+    }// End onBtnDel
+
     onBtnAdd(){
+
         this.btnAdd.addEventListener('click', ev=>{
             let ativ = this.addAtivEl.value;
-            this.addAtivEl.value = '';
-            this.addAtiv(ativ)
+            if(ativ != ''){
+                this.addAtivEl.value = '';
+                this.addAtiv(ativ)  
+            }
         });
+
     }// End onBtnAdd
 
     addAtiv(ativ){
+
         let item = document.createElement('div');
         item.classList.add('item')
 
@@ -25,6 +40,9 @@ class ListController {
         `;
 
         this.listEl.appendChild(item);
+
+        this.btnDel(item);
+
     }// End addAtiv
 
 }
