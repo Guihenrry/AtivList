851e1418a3177fc0177eaadb768a117cef3bd7a2
guihenrry <guilhermehenry60@gmail.com>
2018-09-23 16:31:00 -0300

    add script to add ativ in list

--- script/ListControllers/ListController.js
+++ script/ListControllers/ListController.js
@@ -1,5 +1,30 @@
 class ListController {
 
-	
+	constructor(){
+        this.addAtivEl = document.querySelector('.add-ativ');
+        this.listEl = document.querySelector('.list')
+        this.btnAdd = document.querySelector('.btn-add');
+        this.onBtnAdd();
+    }
+
+    onBtnAdd(){
+        this.btnAdd.addEventListener('click', ev=>{
+            let ativ = this.addAtivEl.value;
+            this.addAtivEl.value = '';
+            this.addAtiv(ativ)
+        });
+    }// End onBtnAdd
+
+    addAtiv(ativ){
+        let item = document.createElement('div');
+        item.classList.add('item')
+
+        item.innerHTML = `
+            <p>${ativ}</p>
+            <button class="btn-del">Excluir</button>
+        `;
+
+        this.listEl.appendChild(item);
+    }// End addAtiv
 
 }
